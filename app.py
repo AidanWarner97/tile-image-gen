cd33434810af68b49566427e0362ba9f8124bdf4
Aidan Warner <aidan@awarner.co.uk>
2024-02-03 16:21:32 +0000

    app.py: Add missing import for Flask
    Signed-off-by: Aidan Warner <aidan@awarner.co.uk>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, send_from_directory
 from PIL import Image, ImageDraw
 import io
 
