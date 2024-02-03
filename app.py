8c509fa79ec3588775f7e0acb31ee14112226f3c
Aidan Warner <aidan@awarner.co.uk>
2024-02-03 16:19:43 +0000

    app.py: Add routes to sitemap and robots directly
    Signed-off-by: Aidan Warner <aidan@awarner.co.uk>

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import io
 
 app = Flask(__name__)
 
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
 @app.route('/')
 def index():
     return render_template('index.html')
